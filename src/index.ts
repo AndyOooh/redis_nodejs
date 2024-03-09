524cf1c874e9c88f4abe6c2d291cd5c017653f93
AndyOoh <andy.oeee@gmail.com>
2024-03-09 20:54:10 +0700

    with-body testing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { rootHandler, helloHandler } from './handlers';
 
 const app = express();
 const port = process.env.PORT || '8000';
 
+app.use(express.json());
+
+const withBodyHandler = (req: Request, res: Response) => {
+  const { body } = req;
+  console.log('🚀  body:', body);
+  return res.json({ message: 'ok' });
+};
+
 app.get('/', rootHandler);
 app.get('/hello/:name', helloHandler);
+app.get('/with-body', withBodyHandler);
 
 app.listen(port, (err?: Error) => {
   if (err) return console.error(err);
